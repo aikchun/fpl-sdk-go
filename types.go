031fecfb16696d8c725b017565387964eca70805
aikchun <iam@simaikchun.com>
2022-01-17 06:16:42 +0800

    Added team types

--- types.go
+++ types.go
@@ -2,10 +2,18 @@ package fpl
 
 type BootstrapStatic struct {
 	Events []Event `json:"events"`
+	Teams  []Team  `json:"teams"`
 }
 
 type Event struct {
 	Id                int64  `json:"id"`
 	Name              string `json:"name"`
 	DeadlineTimeEpoch int64  `json:"deadline_time_epoch"`
+}
+
+type Team struct {
+	Id        int    `json:"id"`
+	Code      int    `json:"code"`
+	Name      string `json:"name"`
+	ShortName string `json:"short_name"`
 }
